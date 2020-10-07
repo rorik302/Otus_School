e2a9c50b32e9e6b1f0f939bc8b67e110b48afbcc
rorik <rorik302@gmail.com>
2020-10-07 16:07:26 +0300

    Courses list page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CoursesListPage from "../pages/CoursesListPage";
+import Navbar from "./Navbar";
 
 class App extends React.Component {
   constructor(props) {
@@ -8,8 +9,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <CoursesListPage />
+      <div className="container">
+        <header>
+          <Navbar/>
+        </header>
+
+        <main>
+          <div className="card shadow">
+            <div className="card-body">
+              <CoursesListPage />
+            </div>
+          </div>
+        </main>
       </div>
     )
   }
--- frontend/src/pages/CoursesListPage.js
+++ frontend/src/pages/CoursesListPage.js
@@ -29,15 +29,36 @@ class CoursesListPage extends React.Component {
 
   render() {
     return (
-      <ul>
-        {this.state.courses.map(course => {
-          return (
-            <li key={course.id}>
-              {course.title}
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <h1>Список курсов</h1>
+
+        <section>
+          {this.state.courses.map(course => {
+            return (
+              <article className="card mb-3 shadow-sm">
+                <div className="card-body">
+                  <h4>{course.title}</h4>
+                  <p>{course.description}</p>
+                  <a href="/courses/:id" className="btn btn-outline-success">Открыть</a>
+                </div>
+                <footer className="card-footer d-flex justify-content-between small">
+                  <span>Продолжительность: {Math.floor(course.duration / 7)} нед.</span>
+                  <span>Дата начала: {course.start_date}</span>
+                </footer>
+              </article>
+            );
+          })}
+        </section>
+      </div>
+      // <ul>
+      //   {this.state.courses.map(course => {
+      //     return (
+      //       <li key={course.id}>
+      //         {course.title}
+      //       </li>
+      //     );
+      //   })}
+      // </ul>
     );
   }
 }
