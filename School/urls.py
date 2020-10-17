dad82cec0172bc37b5bbba2a2fa6858b132721c0
Rorik <rorik302@gmail.com>
2020-10-17 18:49:17 +0300

    catch all urls

--- School/urls.py
+++ School/urls.py
@@ -1,18 +1,14 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from graphene_django.views import GraphQLView
 
 from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('study_app.urls')),
-    path('accounts/', include('users_app.urls')),
-    path('contacts/', include('email_app.urls')),
     path('api/', include('api_app.urls')),
-    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
-    path('', include('frontend.urls'))
+    re_path(r'.*', include('frontend.urls'))
 ]
 
 if settings.DEBUG:
