8e089ce9c110af04fc99f10965e0ae37e852c30e
Rorik <rorik302@gmail.com>
2020-10-18 11:58:36 +0300

    CourseCreate base page

---
+++ frontend/src/pages/CourseCreate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CourseCreate extends React.Component {
+    render() {
+        return (
+            <div>CourseCreate</div>
+        )
+    }
+}
+
+export default CourseCreate
