0fb02250c6434ff4f2f96ea81ee8ed05500414c8
Rorik <rorik302@gmail.com>
2020-10-18 09:37:21 +0300

    cleaning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,33 +3,3 @@ import { render } from 'react-dom';
 import App from "./components/App";
 
 render(<App/>, document.getElementById('app'))
-
-// import axios from 'axios'
-//
-// const HOST = 'http://127.0.0.1:8000'
-//
-// const createLi = (item) => {
-//     const li = document.createElement('li');
-//     li.innerText = item.title;
-//     return li
-// }
-//
-// fetch(`${HOST}/api/study/courses/`)
-//     .then(response => {
-//         response.json().then(res => {
-//             const list = document.querySelector('.courses-list')
-//             res.map((course => {
-//                 list.appendChild(createLi(course))
-//             }))
-//         })
-//     }).catch(error => console.log(error))
-//
-// axios.get(`${HOST}/api/study/lessons/`)
-//     .then(response => {
-//         const list = document.querySelector('.lessons-list')
-//         response.data.map((les) => {
-//             list.appendChild(createLi(les))
-//         })
-//     }).catch(error => console.log(error))
-//
-//
