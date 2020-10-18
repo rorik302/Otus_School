eb87d6ada630f6aa174b00761b5266aa277b4e96
Rorik <rorik302@gmail.com>
2020-10-18 09:39:03 +0300

    cleaning

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import CoursesListPage from "../pages/CoursesListPage";
-import Navbar from "./Navbar";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,18 +7,8 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <header>
-          <Navbar/>
-        </header>
+      <div>
 
-        <main>
-          <div className="card shadow">
-            <div className="card-body">
-              <CoursesListPage />
-            </div>
-          </div>
-        </main>
       </div>
     )
   }
