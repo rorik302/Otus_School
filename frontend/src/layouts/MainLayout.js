dc6e644b9ee7cacd86c2756eab4b51cbd64dcc42
Rorik <rorik302@gmail.com>
2020-10-18 15:06:35 +0300

    Navbar

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 class MainLayout extends React.Component {
     constructor(props) {
@@ -10,22 +10,7 @@ class MainLayout extends React.Component {
         return (
             <>
                 <header>
-                    <nav className="navbar bg-secondary">
-                        <ul className="navbar-nav flex-row">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link btn btn-light">Все курсы</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/courses/create" className="nav-link btn btn-success">Добавить курс</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/contacts" className="nav-link btn btn-light">Контакты</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/login" className="nav-link btn btn-light">Войти</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <Navbar/>
                 </header>
 
                 <main>
