337b9e532cb502eefd4bb9b41da61d1e1bed71ab
Rorik <rorik302@gmail.com>
2020-10-18 11:43:06 +0300

    AppRoute Component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import CoursesList from "../pages/CoursesList";
+import Contacts from "../pages/Contacts";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Login";
+import CourseDetails from "../pages/CourseDetails";
+
+class AppRoute extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {layout: Layout, component: Component, ...rest} = this.props
+
+        return (
+            <Route {...rest} render={(props) => {
+                return (
+                    <Layout>
+                        <Component {...props}/>
+                    </Layout>
+                )
+            }}/>
+        )
+    }
+}
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div>
-
-      </div>
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <AppRoute path="/" exact layout={MainLayout} component={CoursesList}/>
+                <AppRoute path="/courses/:id" layout={MainLayout} component={CourseDetails}/>
+                <AppRoute path="/contacts" exact layout={MainLayout} component={Contacts}/>
+                <AppRoute path="/login" exact layout={AuthLayout} component={Login}/>
+            </Router>
+        )
+    }
 }
 
 export default App
